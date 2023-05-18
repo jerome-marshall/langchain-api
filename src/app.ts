a0fbc5f9ff2d773d581b50d8baa110e403e3584d
Jerome Marshall <jeromemarshall0@gmail.com>
2023-05-18 17:26:07 +0530

    ⏫ UPDATE: port

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,10 @@ const app = express()
 
 app.use(cors<Request>())
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000!")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`)
 
   routes(app)
 })
